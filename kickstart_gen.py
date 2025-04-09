9133b89acdda25f074b2d20182938649a51f616d
9F4R14 <9faria@gmail.com>
2025-04-09 17:22:36 +0100

    questions ans answers

--- kickstart_gen.py
+++ kickstart_gen.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python
+import ipaddress
 import os
 import shutil
 
 virtual = None
 
-ans = input("the installation is virtual or physical?:(y/n) ")
-if ans == "y":
+ans_virt = input("The installation is virtual or physical?:(y/n) ")
+ans_part = input("Full partition config?:(y/n) ")
+if ans_virt.lower() == "y":
     virtual = True
-elif ans == "n":
+elif ans_virt.lower() == "n":
     virtual = False
+else:
+    print("ERROR: invalid input")
+    raise SystemExit(1)
+
+if ans_part.lower() == "y":
+    part = True
+elif ans_part.lower() == "n":
+    part = False
 else:
     print("ERROR: invalid input")
     raise SystemExit(1)
@@ -17,7 +28,25 @@ source_skel = "Skel_Rocky9ks.cfg"
 
 destination_ks = "Rocky9ks.cfg"
 
-if virtual == True:
+if virtual == True and part == False:
+    ans_ipaddr = input("what is the ip address for installation?:(x.x.x.x) ")
+    try:
+        ipaddr = ipaddress.ip_address(ans_ipaddr)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_netmask = input("what is the netmask?:(x.x.x.x) ")
+    try:
+        ipnmask = ipaddress.ip_address(ans_netmask)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_gw = input("what is the gateway?:(x.x.x.x) ")
+    try:
+        ipgw = ipaddress.ip_address(ans_gw)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
     try:
         with open(source_skel, 'r') as file:
             lines = file.readlines()
@@ -25,7 +54,7 @@ if virtual == True:
         print("File Skel_Rocky9ks.cfg not found!")
 
     # Modify the x-th line (0-based index)
-    lines[10] = "network  --bootproto=static --device=ens18 --gateway=192.168.1.1 --ip=192.168.1.114 --nameserver=8.8.8.8,8.8.4.4 --netmask=255.255.255.0 --activate" + "\n"
+    lines[10] = "network  --bootproto=static --device=ens18 --gateway=" + ans_gw + " --ip=" + ans_ipaddr + " --nameserver=8.8.8.8,8.8.4.4 --netmask=" + ans_netmask + " --activate" + "\n"
     
     lines[26] = "ignoredisk --only-use=sda" + "\n"
     lines[28] = "bootloader --append=\" crashkernel=auto\" --location=mbr --boot-drive=sda" + "\n"
@@ -37,12 +66,80 @@ if virtual == True:
     lines[36] = "logvol / --fstype=\"xfs\" --grow --size=1024 --name=root --vgname=rl" + "\n"
     with open(destination_ks, 'w') as file:
         file.writelines(lines)
-elif virtual == False:
-    with open(source_skel, 'r') as file:
-        lines = file.readlines()
+
+elif virtual == True and part == True:
+    ans_ipaddr = input("what is the ip address for installation?:(x.x.x.x) ")
+    try:
+        ipaddr = ipaddress.ip_address(ans_ipaddr)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_netmask = input("what is the netmask?:(x.x.x.x) ")
+    try:
+        ipnmask = ipaddress.ip_address(ans_netmask)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_gw = input("what is the gateway?:(x.x.x.x) ")
+    try:
+        ipgw = ipaddress.ip_address(ans_gw)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    try:
+        with open(source_skel, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print("File Skel_Rocky9ks.cfg not found!")
 
     # Modify the x-th line (0-based index)
-    lines[10] = "network  --bootproto=static --device=ens18 --gateway=192.168.1.1 --ip=192.168.1.114 --nameserver=8.8.8.8,8.8.4.4 --netmask=255.255.255.0 --activate" + "\n"
+    lines[10] = "network  --bootproto=static --device=ens18 --gateway=" + ans_gw + " --ip=" + ans_ipaddr + " --nameserver=8.8.8.8,8.8.4.4 --netmask=" + ans_netmask + " --activate" + "\n"
+
+    lines[41] = "ignoredisk --only-use=nvme0n1" + "\n"
+    lines[43] = "clearpart --none --initlabel" + "\n"
+    lines[45] = "part /boot/efi --fstype=\"efi\" --ondisk=nvme0n1 --size=5124 --fsoptions=\"umask=0077,shortname=winnt\"" + "\n"
+    lines[46] = "part /boot --fstype=\"xfs\" --ondisk=nvme0n1 --size=512" + "\n"
+    lines[47] = "part pv.560 --fstype=\"lvmpv\" --ondisk=nvme0n1 --size=799752" + "\n"
+    lines[48] = "volgroup rl --pesize=4096 pv.560" + "\n"
+    lines[49] = "logvol /opt --fstype=\"xfs\" --size=225280 --name=opt --vgname=rl" + "\n"
+    lines[50] = "logvol /app --fstype=\"xfs\" --size=358400 --name=app --vgname=rl" + "\n"
+    lines[51] = "logvol swap --fstype=\"swap\" --size=6144 --name=swap --vgname=rl" + "\n"
+    lines[52] = "logvol / --fstype=\"xfs\" --size=153600 --name=root --vgname=rl" + "\n"
+    lines[53] = "logvol /tmp --fstype=\"xfs\" --size=5120 --name=tmp --vgname=rl" + "\n"
+    lines[54] = "logvol /home --fstype=\"xfs\" --size=51200 --name=home --vgname=rl" + "\n"
+    with open(destination_ks, 'w') as file:
+        file.writelines(lines)
+
+elif virtual == False:
+    ans_ipaddr = input("what is the ip address for installation?:(x.x.x.x) ")
+    try:
+        ipaddr = ipaddress.ip_address(ans_ipaddr)
+        ipaddress.ip_network
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_netmask = input("what is the netmask?:(x.x.x.x) ")
+    try:
+        ipnmask = ipaddress.ip_address(ans_netmask)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_gw = input("what is the gateway?:(x.x.x.x) ")
+    try:
+        ipgw = ipaddress.ip_address(ans_gw)
+    except ValueError:
+        print("ERROR: invalid input")
+        raise SystemExit(1)
+    ans_card = input("what is the card interface?:(ex.ens18) ")
+    
+    try:
+        with open(source_skel, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print("File Skel_Rocky9ks.cfg not found!")
+    
+    # Modify the x-th line (0-based index)
+    lines[10] = "network  --bootproto=static --device=" + ans_card + " --gateway=" + ans_gw + " --ip=" + ans_ipaddr + " --nameserver=8.8.8.8,8.8.4.4 --netmask=" + ans_netmask + " --activate" + "\n"
 
     lines[41] = "ignoredisk --only-use=nvme0n1" + "\n"
     lines[43] = "clearpart --none --initlabel" + "\n"
